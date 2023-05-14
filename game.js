aaa6168207b4f56b43a8d5cd7e115e47efaf556f
unknown <nehemimosqueda8@gmail.com>
2023-05-13 23:37:38 -0300

    adicion de mejoras y limpieza de código

--- game.js
+++ game.js
@@ -1,94 +1,98 @@
+
+
 export class Game extends Phaser.Scene {
-    
+
     constructor(){
         super({key: 'game'})
     }
-
+    
     init() {
         this.score = 0;
-
+    
     }
     
     preload ()
     {
-        this.load.image('character', './assets/image/character.png');
+        this.load.image('ship', './assets/image/ship.png');
         this.load.image('background', './assets/image/background.png');
-        this.load.image('enemy', './assets/image/enemy.png');
-        this.load.image('weapon', './assets/image/weapon.png');
+        this.load.image('asteroid', './assets/image/asteroid.png');
     }
     
     create ()
     {
         this.add.image(this.game.config.width / 2, this.game.config.height / 2, 'background');
-
-
     
-        this.objeto = this.physics.add.group();
-        this.objeto.create(100, -10, 'enemy');
-        this.objeto.create(200, -10, 'enemy');
-        this.objeto.create(300, -10, 'enemy');
-        this.objeto.create(400, -10, 'enemy');
-        this.objeto.create(500, -10, 'enemy');
-        this.objeto.create(600, -10, 'enemy');
-        this.objeto.create(700, -10, 'enemy');
-
+    
+    
+        this.asteroid = this.physics.add.group();
+        this.asteroid.create(100, -10, 'asteroid');
+        this.asteroid.create(200, -10, 'asteroid');
+        this.asteroid.create(300, -10, 'asteroid');
+        this.asteroid.create(400, -10, 'asteroid');
+        this.asteroid.create(500, -10, 'asteroid');
+        this.asteroid.create(600, -10, 'asteroid');
+        this.asteroid.create(700, -10, 'asteroid');
+        this.asteroid.create(450, -10, 'asteroid');
+        this.asteroid.create(550, -10, 'asteroid');
+        this.asteroid.create(650, -10, 'asteroid');
+    
         this.scoreText = this.add.text(16, 16, 'Points: 0', { 
             fontSize: '20px', 
             fill: '#fff', 
             fontFamily: 'verdana, arial, sans-serif' 
         });
     
-        this.platform = this.physics.add.sprite(400, 450, 'character');
-        this.platform.body.allowGravity= false;
-        this.platform.setCollideWorldBounds(true);
+        this.ship = this.physics.add.sprite(400, 450, 'ship');
+        this.ship.body.allowGravity= false;
+        this.ship.setCollideWorldBounds(true);
         
     
-        this.physics.add.collider (this.objeto, this.platform, this.platformCrash.bind(this), null, );
-
+        this.physics.add.collider (this.asteroid, this.ship, this.shipCrash.bind(this), null, );
+    
         this.cursors = this.input.keyboard.createCursorKeys();
     
         // Establecer la velocidad máxima y la velocidad actual
-        this.maxEnemySpeed = 12;
-        this.enemySpeed = 4;
-    }
-
-    platformCrash() {
-        this.scene.start('gameover');
+        
+        this.asteroidSpeed = 4;
     }
     
-
+    shipCrash() {
+        this.scene.start('gameover', { score: this.score });
+    }
+    
+    
     update ()
     {
         if (this.cursors.left.isDown) {
-            this.platform.setAngle(-20);
-            this.platform.setVelocityX(-300);
+            this.ship.setAngle(-20);
+            this.ship.setVelocityX(-300);
         } else if (this.cursors.right.isDown) {
-            this.platform.setAngle(20);
-            this.platform.setVelocityX(300);
+            this.ship.setAngle(20);
+            this.ship.setVelocityX(300);
         } else {
-            this.platform.setAngle(0);
-            this.platform.setVelocityX(0);
+            this.ship.setAngle(0);
+            this.ship.setVelocityX(0);
         }
     
-        // Hacer que los enemigos caigan hacia abajo y se teletransporten al inicio de la pantalla si salen de ella
-        this.objeto.children.iterate(function(enemy) {
-            if (enemy.y > 600) {
-                // Generar una nueva posición aleatoria y establecerla como la nueva posición del enemigo
-                enemy.x = Phaser.Math.Between(0, 800);
-                enemy.y = -10;
-
-                this.score += 1;
+        // Hacer que los asteroides caigan hacia abajo y se teletransporten al inicio de la pantalla si salen de ella
+        this.asteroid.children.iterate(function(asteroid) {
+            if (asteroid.y > 600) {
+                // Generar una nueva posición aleatoria y establecerla como la nueva posición del asteroide
+                asteroid.x = Phaser.Math.Between(0, 800);
+                asteroid.y = -10;
+    
+                this.score = this.score + 1 ;
                 this.scoreText.setText('Points: ' + this.score);
                 
-                    // Aumentar la velocidad cada vez que un enemigo se teletransporta
-                    this.enemySpeed += 0.04;
+                    // Aumentar la velocidad cada vez que un asteroide se teletransporta
+                    this.asteroidSpeed += 0.04;
             
             } else {
-                enemy.y += this.enemySpeed;
+                asteroid.y += this.asteroidSpeed;
             }
         }, this);// Pase 'this' como segundo parámetro para que funcione dentro de la función de iteración
-
+    
         
         
     }
-}
+}    
